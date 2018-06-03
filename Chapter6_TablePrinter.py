6ce8be72a9f8d2012ec098f8db133163fa8256c8
Stanley Quon <stanley_quon@intuit.com>
2018-06-03 11:40:09 +0100

    Chapter 6 practice project

--- Chapter6_TablePrinter.py
+++ Chapter6_TablePrinter.py
@@ -1,13 +1,81 @@
 #! /usr/bin/python3
 #
 
+fDebug=0
+
 headerData = ['fruits', 'names', 'animals']
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-print (tableData [0][0])
-print (tableData [0][1])
-print (tableData [0][2])
-print (tableData [0][3])
+def printTable (tableData, headerData):
+   #
+   # Confirm header columns match data columns
+   #
+   if (len (headerData) != len (tableData)):
+      print ()
+      print ("Header data doesn't match column count")
+
+      return 1
+
+   columnWidths = [0] * len (tableData)
+
+   #
+   # Determine the required column width
+   #
+   for counter in range (0, len (headerData)):
+      columnWidths [counter] = len (headerData [counter])
+
+   nColumn = 0
+   for innerList in tableData:
+      for element in innerList:
+         elementWidth = len (element)
+
+         if (elementWidth > columnWidths [nColumn]):
+            columnWidths [nColumn] = elementWidth
+
+      nColumn += 1
+
+   nColumn = 0
+   szLine = ''
+   for header in headerData:
+      szLine += header.rjust (columnWidths [nColumn]) + ' '
+      nColumn += 1
+
+   print ()
+   if (fDebug == 0):
+      print (szLine.rstrip ())
+   else:
+      print ("->" + szLine.rstrip () + "<-")
+
+   szLine = ''
+   for width in columnWidths:
+      szLine += "-" * width + ' '
+
+   if (fDebug == 0):
+      print (szLine.rstrip ())
+   else:
+      print ("->" + szLine.rstrip () + "<-")
+
+   rowCount = len (tableData [0])
+   columnCount = len (tableData)
+
+   for column in range (0, rowCount):
+      szLine = ''
+      for row in range (0, columnCount):
+         szLine += tableData [row][column].rjust (columnWidths [row]) + ' '
+
+      if (fDebug == 0):
+         print (szLine.rstrip ())
+      else:
+         print ("->" + szLine.rstrip () + "<-")
+
+   return 0
+
+nReturn = printTable (tableData, headerData)
+
+if (nReturn != 0):
+   print ()
+   print ("Error trying to print table")
 
